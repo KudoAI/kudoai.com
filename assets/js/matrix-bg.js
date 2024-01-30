aff4b767cc193ea64913fb688f52ca06734a568e
adamlui@protonmail.com <adamlui@protonmail.com>
2024-01-29 20:05:36 -0800

    Added color cycling + whitespace

--- assets/js/matrix-bg.js
+++ assets/js/matrix-bg.js
@@ -1,12 +1,16 @@
-import * as datGui from "https://cdn.skypack.dev/dat.gui@0.7.7"
+import * as datGui from "https://cdn.skypack.dev/dat.gui@0.7.7";
 
-const state = { fps: 13, color: '#878e87', charset: '0123456789ABCDEF', size: 12 },
-      gui = new datGui.GUI(),
+const state = {
+    fps: 13, size: 12, charset: '0123456789ABCDEF', secDelayTilColorChange: 5,
+    hue: 120, saturation: 3, lightness: 54 // init at #878e87 / HSL(120, 3%, 54.31%)
+}
+
+const gui = new datGui.GUI(),
       fpsCtrl = gui.add(state, 'fps').min(1).max(120).step(1),
       sizeCtrl = gui.add(state, 'size').min(1).max(120).step(1),
       canvas = document.querySelector('canvas'),
       ctx = canvas.getContext('2d')
-gui.addColor(state, 'color') ; gui.add(state, 'charset')
+gui.add(state, 'charset')
 
 let w, h, p
 const resize = () => {
@@ -21,10 +25,14 @@ resize()
 const random = items => items[Math.floor(Math.random() * items.length)]
 
 const draw = () => {
-    ctx.fillStyle = 'rgba(0,0,0,.05)'
-    ctx.fillRect(0, 0, w, h)
-    ctx.fillStyle = state.color
-    ctx.font = state.size + 'px sans-serif'
+    if (Date.now() - startTime >= state.secDelayTilColorChange*1000) { // begin transition thru colors
+        state.hue = (state.hue + 1) % 360
+        state.saturation = 100 + Math.sin((state.hue * Math.PI) / 180) * 50
+        state.lightness = 50 + Math.cos((state.hue * Math.PI) / 180) * 25
+    }
+    const rgbColor = `hsl(${ state.hue }, ${ state.saturation }%, ${ state.lightness }%)`
+    ctx.fillStyle = 'rgba(0,0,0,.05)' ; ctx.fillRect(0, 0, w, h)
+    ctx.fillStyle = rgbColor ; ctx.font = state.size + 'px sans-serif'
     for (let i = 0; i < p.length; i++) {
         let v = p[i]
         ctx.fillText(random(state.charset), i * state.size, v)
@@ -32,8 +40,9 @@ const draw = () => {
     }
 }
 
-let interval = setInterval(draw, 1000 / state.fps)
+let startTime = Date.now(),
+    interval = setInterval(draw, 1000 / state.fps)
 fpsCtrl.onFinishChange(fps => {
-  if (interval) clearInterval(interval)
-  interval = setInterval(draw, 1000 / fps)
-});
+    if (interval) clearInterval(interval)
+    interval = setInterval(draw, 1000 / fps)
+})
