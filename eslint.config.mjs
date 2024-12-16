0471563bb2d2beef63f28a69b723d6122b4b6863
adamlui@protonmail.com <adamlui@protonmail.com>
2024-12-16 06:07:56 -0800

    Added URL pattern to `import/no-unresolved` ignorelist to avoid false positives (https://github.com/import-js/eslint-plugin-import/issues/3118#issuecomment-2545314503)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,9 @@ export default [
             'no-empty': 'off', // allow empty blocks
             'no-inner-declarations': 'off', // allow function declarations anywhere
             'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
-            'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
+            'no-unused-vars': ['error', { 'caughtErrors': 'none' }], // allow unused named args in catch blocks
+            'import/no-unresolved': ['error', { ignore: ['https?://'] }] // enforce ESM import resolution
+                // + avoid false positives for URLs (https://github.com/import-js/eslint-plugin-import/issues/3118#issuecomment-2545314503)
         }
     },
     { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended },
