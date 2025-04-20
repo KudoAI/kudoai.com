23b40ffed0f8ffb90b33ec93640da12e0985b199
Adam Lui <adam@kudoai.com>
2025-04-20 09:17:54 -0700

    Moved copyright year insertion to footer-hacks.js, removed year pre-hack, added fade badges

---
+++ assets/js/src/footer-hacks.js
@@ -0,0 +1,13 @@
+// Insert copyright year
+document.querySelector('.copyright').innerHTML = `© 2023—${new Date().getFullYear()} KudoAI`
+
+// Fade-in badges + add hover listeners
+document.addEventListener('DOMContentLoaded', () => {
+    const finalOpacity = 0.8
+    document.querySelectorAll('.badges img').forEach(badge => {
+        badge.style.transition = 'opacity 2s ease-in-out' ; badge.style.opacity = finalOpacity
+        badge.onmouseenter = () => {
+            badge.style.transition = 'opacity 0.2s ease' ; badge.style.opacity = 1 }
+        badge.onmouseleave = () => badge.style.opacity = finalOpacity
+    })
+})
