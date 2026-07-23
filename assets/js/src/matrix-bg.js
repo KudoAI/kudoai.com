eaaafcea7b4d0cdbf63fe31d807ef0659f3c48c8
Adam Lui <adam@kudoai.com>
2026-07-23 11:47:04 -0700

    Lowered fps + opacity (closes KudoAI/kudoai.com#307)

--- assets/js/src/matrix-bg.js
+++ assets/js/src/matrix-bg.js
@@ -1,7 +1,7 @@
 import datGui from 'https://cdn.skypack.dev/dat.gui@0.7.7'
 
 const state = {
-    fps: 13, size: 12, charset: '0123456789ABCDEF', secDelayTilColorChange: 10,
+    fps: 9, size: 12, charset: '0123456789ABCDEF', secDelayTilColorChange: 10,
     hue: 120, saturation: 3, lightness: 54 // init at #878e87 / HSL(120, 3%, 54.31%)
 }
 
@@ -27,13 +27,13 @@ const random = items => items[Math.floor(Math.random() * items.length)]
 const draw = () => {
     if (Date.now() - startTime >= state.secDelayTilColorChange*1000) { // begin transition thru colors
         state.hue = (state.hue + 1) % 360
-        state.saturation = 100 + Math.sin((state.hue * Math.PI) / 180) * 50
-        state.lightness = 50 + Math.cos((state.hue * Math.PI) / 180) * 25
+        state.saturation = 100 + Math.sin((state.hue * Math.PI) / 180) *50
+        state.lightness = 50 + Math.cos((state.hue * Math.PI) / 180) *25
     }
-    const rgbColor = `hsl(${ state.hue }, ${ state.saturation }%, ${ state.lightness }%)`
+    const rgbColor = `hsl(${ state.hue }, ${ state.saturation }%, ${ state.lightness *0.65 }%)`
     ctx.fillStyle = 'rgba(0,0,0,.05)' ; ctx.fillRect(0, 0, w, h)
     ctx.fillStyle = rgbColor ; ctx.font = state.size + 'px sans-serif'
-    for (let i = 0; i < p.length; i++) {
+    for (let i = 0 ; i < p.length ; i++) {
         let v = p[i]
         ctx.fillText(random(state.charset), i * state.size, v)
         p[i] = v >= h || v >= 10000 * Math.random() ? 0 : v + state.size
